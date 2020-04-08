f031f53a6d9ce144d7fbfb66eede59edd4e47f6f
Dmitry Levchenko <wirwl@hotmail.com>
2020-04-08 15:05:59 +0600

    Check code with FSD standarts: add js prefix

--- SRC/components/select-items/select-items.ts
+++ SRC/components/select-items/select-items.ts
@@ -1,14 +1,14 @@
 $(document).ready(() => {
-    const $selectItems = $('.select-items');
+    const $selectItems = $('.js-select-items');
     $selectItems.each(function() {
         const $si = $(this);
-        const $buttonAdd = $si.find('.select-items__button-add');
-        const $buttonRemove = $si.find('.select-items__button-remove');
-        const $select = $si.find('.select-items__options');
+        const $buttonAdd = $si.find('.js-select-items__button-add');
+        const $buttonRemove = $si.find('.js-select-items__button-remove');
+        const $select = $si.find('.js-select-items__options');
         const select = $select[0] as HTMLSelectElement;
         $buttonAdd.click(function() {
             const randomValue = Math.random();
-            const item = prompt('Введите новый обьект', (~~(randomValue * 1000)).toString());
+            const item = prompt('Введите новый объект', (~~(randomValue * 1000)).toString());
             if (item) $select.append(new Option(item));
 
             if (select.length > 1) $si.removeClass('select-items_not-using');
