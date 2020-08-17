52b9feec5e08446e40466588b94f9ebcc2f6d942
wirwl <wirwl@hotmail.com>
2020-08-17 14:19:07 +0600

    Remove IIFE from toxin-rangeslider.ts #7.4

--- SRC/components/toxin-rangeslider/toxin-rangeslider.ts
+++ SRC/components/toxin-rangeslider/toxin-rangeslider.ts
@@ -2,21 +2,19 @@ import TRSPresenter from './core/presenter';
 import TRSView from './core/view';
 import TRSModel from './core/model';
 
-// eslint-disable-next-line prettier/prettier
-;(function($) {
-    $.fn.toxinRangeSlider = function(this: JQuery<HTMLElement>, options: RangeSliderOptions): JQuery {
-        this.each(function() {
-            if (!$.data(this, 'toxinRangeSlider')) {
-                $.data(this, 'toxinRangeSlider', new TRSPresenter(new TRSModel(options), new TRSView($(this))));
-            }
-        });
-        return this;
-    };
-
-    $.fn.toxinRangeSlider.options = TRSModel.defaults;
-
-    const el = $('.toxin-rangeslider-here');
-    el.each(function() {
-        $(this).toxinRangeSlider();
+$.fn.toxinRangeSlider = function (this: JQuery<HTMLElement>, options: RangeSliderOptions): JQuery {
+    this.each(function () {
+        if (!$.data(this, 'toxinRangeSlider')) {
+            $.data(this, 'toxinRangeSlider', new TRSPresenter(new TRSModel(options), new TRSView($(this))));
+        }
     });
-})(jQuery);
+    return this;
+};
+
+$.fn.toxinRangeSlider.options = TRSModel.defaults;
+
+const el = $('.toxin-rangeslider-here');
+el.each(function () {
+    $(this).toxinRangeSlider();
+});
+
