bb942d7d013d66cef552a88b3f0fae9756eef9da
Dmitry <lds84pro@gmail.com>
2021-02-17 13:03:30 +0600

    Refactoring: split the validate method into separate functions #19

--- SRC/components/toxin-rangeslider/core/model.ts
+++ SRC/components/toxin-rangeslider/core/model.ts
@@ -11,22 +11,24 @@ class TRSModel {
 
   constructor(options?: AnyObject) {
     this.settings = $.extend(true, {}, TRSModel.defaults);
-    // $.extend(true, this.settings, options);
     options && mergeSliderOptions(this.settings, options);
     this.validate();
   }
 
+  updateState(data: AnyObject): void {
+    mergeSliderOptions(this.settings, data);
+    this.validate();
+  }
+
   updateHandleState({ isFromHandle, relValue }: HandleMovingResult): void {
-    // const {
-    //   valueFrom,
-    //   valueTo,
-    //   items: { indexFrom, indexTo, values },
-    // } = this.settings;
+    const {
+      items: { values },
+    } = this.settings;
 
     let index = -1;
     if (this.isUsingItems()) {
-      for (let i = 0; i < this.settings.items.values.length; i += 1) {
-        if (relValue.toString() === this.settings.items.values[i].toString()) {
+      for (let i = 0; i < values.length; i += 1) {
+        if (relValue.toString() === values[i].toString()) {
           index = i;
           break;
         }
@@ -35,22 +37,13 @@ class TRSModel {
     if (isFromHandle) {
       if (this.isUsingItems()) {
         this.settings.items.indexFrom = index;
-        this.settings.valueFrom = this.settings.items.values[index];
+        this.settings.valueFrom = values[index];
       } else this.settings.valueFrom = relValue;
     } else if (this.isUsingItems()) {
       this.settings.items.indexTo = index;
-      this.settings.valueTo = this.settings.items.values[index];
+      this.settings.valueTo = values[index];
     } else this.settings.valueTo = relValue;
 
-    this.validate();
-  }
-
-  isUsingItems(): boolean {
-    return this.settings.items.values.length > 1;
-  }
-
-  updateState(data: AnyObject): void {
-    mergeSliderOptions(this.settings, data);
     this.validate();
   }
 
@@ -58,7 +51,17 @@ class TRSModel {
     return this.settings;
   }
 
-  roundIsStepValueDefined(): void {
+  onHandlePositionChange(data: HandleMovingResult): void {
+    const { isFromHandle } = data;
+    const validatedRelValue = isFromHandle ? this.settings.valueFrom : this.settings.valueTo;
+    this.settings.onHandlePositionChange?.call({ ...this.settings }, { isFromHandle, relValue: validatedRelValue });
+  }
+
+  private isUsingItems(): boolean {
+    return this.settings.items.values.length > 1;
+  }
+
+  private validateIsStepValueDefined(): void {
     const { stepValue } = this.settings;
 
     const isHasStepValue = stepValue > 1;
@@ -90,94 +93,124 @@ class TRSModel {
     }
   }
 
-  onHandlePositionChange(data: HandleMovingResult): void {
-    const { isFromHandle } = data;
-    const validatedRelValue = isFromHandle ? this.settings.valueFrom : this.settings.valueTo;
-    this.settings.onHandlePositionChange?.call({ ...this.settings }, { isFromHandle, relValue: validatedRelValue });
+  private validateItemsValues(): void {
+    const { items, stepValue, isTwoHandles, isVertical, isTip } = this.settings;
+    const indexFrom = items?.indexFrom;
+    const indexTo = items?.indexTo;
+    const values = items?.values;
+
+    // eslint-disable-next-line prefer-destructuring
+    this.settings.minValue = values[0];
+    this.settings.maxValue = values[values.length - 1];
+    this.settings.valueFrom = values[indexFrom];
+    this.settings.valueTo = values[indexTo];
+
+    if (typeof indexFrom === 'undefined') this.settings.items.indexFrom = 0;
+    if (typeof indexTo === 'undefined') this.settings.items.indexTo = values.length - 1;
+
+    if (indexTo > values.length - 1) {
+      this.settings.items.indexTo = values.length - 1;
+    }
+
+    if (isTwoHandles) {
+      if (indexFrom > indexTo) {
+        this.settings.items.indexFrom = indexTo;
+      }
+      if (indexFrom < 0) this.settings.items.indexFrom = 0;
+    }
   }
 
-  private validate(): void {
-    const { items, stepValue, isTwoHandles, isVertical, isTip } = this.settings;
+  private applyDefaultValuesForUndefinedValues() {
+    const { items, stepValue, isTwoHandles, isVertical, isTip, minValue, maxValue, valueFrom, valueTo } = this.settings;
 
     const indexFrom = items?.indexFrom;
     const indexTo = items?.indexTo;
     const values = items?.values;
     const isUsingItems = items && items.values.length > 1;
 
-    this.roundIsStepValueDefined();
+    if (typeof isVertical !== 'boolean') this.settings.isVertical = TRSModel.defaults.isVertical;
+    if (typeof isTwoHandles !== 'boolean') this.settings.isTwoHandles = TRSModel.defaults.isTwoHandles;
+    if (typeof isTip !== 'boolean') this.settings.isTip = TRSModel.defaults.isTip;
+    if (Number.isNaN(minValue)) this.settings.minValue = Number(TRSModel.defaults.minValue);
+    if (Number.isNaN(maxValue)) this.settings.maxValue = Number(TRSModel.defaults.maxValue);
+    if (Number.isNaN(valueFrom)) this.settings.valueFrom = Number(TRSModel.defaults.valueFrom);
+    if (Number.isNaN(valueTo)) this.settings.valueTo = Number(TRSModel.defaults.valueTo);
+    if (Number.isNaN(stepValue)) this.settings.stepValue = TRSModel.defaults.stepValue;
+    if (typeof items !== 'object') this.settings.items = TRSModel.defaults.items;
+  }
 
-    if (isUsingItems) {
-      // eslint-disable-next-line prefer-destructuring
-      this.settings.minValue = values[0];
-      this.settings.maxValue = values[values.length - 1];
-      this.settings.valueFrom = values[indexFrom];
-      this.settings.valueTo = values[indexTo];
+  validateMinAndMaxValues(): void {
+    const { items, stepValue, isTwoHandles, isVertical, isTip } = this.settings;
+    let { valueFrom, valueTo } = this.settings;
 
-      if (typeof indexFrom === 'undefined') this.settings.items.indexFrom = 0;
-      if (typeof indexTo === 'undefined') this.settings.items.indexTo = values.length - 1;
+    const indexFrom = items?.indexFrom;
+    const indexTo = items?.indexTo;
+    const values = items?.values;
+    const isUsingItems = items && items.values.length > 1;
 
-      if (indexTo > values.length - 1) {
-        this.settings.items.indexTo = values.length - 1;
-      }
+    let maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.settings.maxValue);
+    let minValue = Number(isUsingItems ? items.values[0] : this.settings.minValue);
 
-      if (isTwoHandles) {
-        if (indexFrom > indexTo) {
-          this.settings.items.indexFrom = indexTo;
-        }
-        if (indexFrom < 0) this.settings.items.indexFrom = 0;
-      }
-    } else {
-      let maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.settings.maxValue);
-      let minValue = Number(isUsingItems ? items.values[0] : this.settings.minValue);
-      let { valueFrom } = this.settings;
-      let { valueTo } = this.settings;
+    if (minValue === maxValue) {
+      this.settings.valueFrom = minValue;
+      valueFrom = this.settings.valueFrom;
+      this.settings.maxValue = minValue + 1;
+      maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.settings.maxValue);
+      this.settings.valueTo = maxValue;
+      valueTo = maxValue;
+    }
 
-      if (typeof isVertical !== 'boolean') this.settings.isVertical = TRSModel.defaults.isVertical;
-      if (typeof isTwoHandles !== 'boolean') this.settings.isTwoHandles = TRSModel.defaults.isTwoHandles;
-      if (typeof isTip !== 'boolean') this.settings.isTip = TRSModel.defaults.isTip;
-      if (Number.isNaN(minValue)) this.settings.minValue = Number(TRSModel.defaults.minValue);
-      if (Number.isNaN(maxValue)) this.settings.maxValue = Number(TRSModel.defaults.maxValue);
-      if (Number.isNaN(valueFrom)) this.settings.valueFrom = Number(TRSModel.defaults.valueFrom);
-      if (Number.isNaN(valueTo)) this.settings.valueTo = Number(TRSModel.defaults.valueTo);
-      if (Number.isNaN(stepValue)) this.settings.stepValue = TRSModel.defaults.stepValue;
-      if (typeof items !== 'object') this.settings.items = TRSModel.defaults.items;
+    if (maxValue < minValue) {
+      this.settings.minValue = maxValue;
+      this.settings.maxValue = minValue;
+      minValue = maxValue;
+      maxValue = minValue;
+      if (valueFrom < minValue || valueFrom > maxValue) this.settings.valueFrom = minValue;
 
-      if (minValue === maxValue) {
-        this.settings.valueFrom = minValue;
+      if (valueTo < minValue || valueTo > maxValue) this.settings.valueTo = maxValue;
+    }
+  }
+
+  private validateStepValue(): void {
+    const { maxValue, minValue, stepValue } = this.settings;
+
+    const size = Number(maxValue) - Number(minValue);
+    if (stepValue < 1) this.settings.stepValue = 1;
+    if (this.settings.stepValue > size) this.settings.stepValue = size;
+  }
+
+  private validateHandlesValue(): void {
+    const { items, stepValue, isTwoHandles, isVertical, isTip, minValue, maxValue } = this.settings;
+    let { valueFrom, valueTo } = this.settings;
+
+    if (isTwoHandles) {
+      if (valueFrom > valueTo) {
+        this.settings.valueFrom = valueTo;
+        this.settings.valueTo = valueFrom;
         valueFrom = this.settings.valueFrom;
-        this.settings.maxValue = minValue + 1;
-        maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.settings.maxValue);
-        this.settings.valueTo = maxValue;
-        valueTo = maxValue;
+        valueTo = this.settings.valueTo;
       }
+      if (valueTo < valueFrom) this.settings.valueTo = valueFrom;
 
-      if (maxValue < minValue) {
-        this.settings.minValue = maxValue;
-        this.settings.maxValue = minValue;
-        minValue = maxValue;
-        maxValue = minValue;
-        if (valueFrom < minValue || valueFrom > maxValue) this.settings.valueFrom = minValue;
+      if (valueFrom < minValue) this.settings.valueFrom = minValue;
+    } else if (valueTo < minValue) this.settings.valueTo = minValue;
 
-        if (valueTo < minValue || valueTo > maxValue) this.settings.valueTo = maxValue;
-      }
+    if (valueTo > maxValue) this.settings.valueTo = maxValue;
+  }
 
-      const size = maxValue - minValue;
-      if (stepValue < 1) this.settings.stepValue = 1;
-      if (this.settings.stepValue > size) this.settings.stepValue = size;
+  private validate(): void {
+    this.applyDefaultValuesForUndefinedValues();
 
-      if (isTwoHandles) {
-        if (valueFrom > valueTo) {
-          this.settings.valueFrom = valueTo;
-          this.settings.valueTo = valueFrom;
-          valueFrom = this.settings.valueFrom;
-          valueTo = this.settings.valueTo;
-        }
-        if (valueTo < valueFrom) this.settings.valueTo = valueFrom;
+    this.validateIsStepValueDefined();
 
-        if (valueFrom < minValue) this.settings.valueFrom = minValue;
-      } else if (valueTo < minValue) this.settings.valueTo = minValue;
+    if (this.isUsingItems()) {
+      this.validateItemsValues();
+    } else {
+      this.validateMinAndMaxValues();
 
-      if (valueTo > maxValue) this.settings.valueTo = maxValue;
+      this.validateStepValue();
+
+      this.validateHandlesValue();
     }
   }
 }
