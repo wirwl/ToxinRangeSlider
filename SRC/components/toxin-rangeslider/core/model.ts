3f54d40fdb02825ce8e7a6e56d82c4390393f2de
Dmitry <lds84pro@gmail.com>
2021-02-18 21:21:42 +0600

    Add public methods for get/set state values in model and presenter layer #18 #21

--- SRC/components/toxin-rangeslider/core/model.ts
+++ SRC/components/toxin-rangeslider/core/model.ts
@@ -2,69 +2,23 @@ import defaultRangeSliderState from './defaults';
 import { mergeSliderOptions } from './utils';
 
 class TRSModel {
-  private settings: RangeSliderOptions;
+  private state: RangeSliderOptions;
 
   static readonly defaults: RangeSliderOptions = $.extend(true, {}, defaultRangeSliderState);
 
   constructor(options?: AnyObject) {
-    this.settings = $.extend(true, {}, TRSModel.defaults);
-    mergeSliderOptions(this.settings, options || {});
+    this.state = $.extend(true, {}, TRSModel.defaults);
+    mergeSliderOptions(this.state, options || {});
     this.validate();
-  }
-
-  updateState(data: AnyObject): void {
-    mergeSliderOptions(this.settings, data);
-    this.validate();
-  }
-
-  updateHandleState({ isFromHandle, relValue }: HandleMovingResult): void {
-    const {
-      items: { values },
-    } = this.settings;
-
-    let index = -1;
-    if (this.isUsingItems()) {
-      for (let i = 0; i < values.length; i += 1) {
-        if (relValue.toString() === values[i].toString()) {
-          index = i;
-          break;
-        }
-      }
-    }
-    if (isFromHandle) {
-      if (this.isUsingItems()) {
-        this.settings.items.indexFrom = index;
-        this.settings.valueFrom = values[index];
-      } else this.settings.valueFrom = relValue;
-    } else if (this.isUsingItems()) {
-      this.settings.items.indexTo = index;
-      this.settings.valueTo = values[index];
-    } else this.settings.valueTo = relValue;
-
-    this.validate();
-  }
-
-  getState(): RangeSliderOptions {
-    return this.settings;
-  }
-
-  onHandlePositionChange(data: HandleMovingResult): void {
-    const { isFromHandle } = data;
-    const validatedRelValue = isFromHandle ? this.settings.valueFrom : this.settings.valueTo;
-    this.settings.onHandlePositionChange?.call({ ...this.settings }, { isFromHandle, relValue: validatedRelValue });
-  }
-
-  private isUsingItems(): boolean {
-    return this.settings.items?.values?.length > 1;
   }
 
   private validateIsStepValueDefined(): void {
-    const { stepValue } = this.settings;
+    const { stepValue } = this.state;
 
     const isHasStepValue = stepValue > 1;
 
     if (isHasStepValue) {
-      const { valueFrom, valueTo, minValue, maxValue } = this.settings;
+      const { valueFrom, valueTo, minValue, maxValue } = this.state;
       const relLengthTotal = Number(maxValue) - Number(minValue);
       const stepCountTotal = Math.ceil(relLengthTotal / stepValue);
       const remainderValue = relLengthTotal % stepValue;
@@ -85,105 +39,106 @@ class TRSModel {
         return nextRightValue;
       };
 
-      this.settings.valueFrom = roundHandleValue(Number(valueFrom));
-      this.settings.valueTo = roundHandleValue(Number(valueTo));
+      this.state.valueFrom = roundHandleValue(Number(valueFrom));
+      this.state.valueTo = roundHandleValue(Number(valueTo));
     }
   }
 
   private validateItemsValues(): void {
-    const { items, isTwoHandles } = this.settings;
+    const { items, isTwoHandles } = this.state;
     const indexFrom = items?.indexFrom;
     const indexTo = items?.indexTo;
     const values = items?.values;
 
-    [this.settings.minValue] = values;
-    this.settings.maxValue = values[values.length - 1];
-    this.settings.valueFrom = values[indexFrom];
-    this.settings.valueTo = values[indexTo];
+    [this.state.minValue] = values;
+    this.state.maxValue = values[values.length - 1];
+    this.state.valueFrom = values[indexFrom];
+    this.state.valueTo = values[indexTo];
 
-    if (typeof indexFrom === 'undefined') this.settings.items.indexFrom = 0;
-    if (typeof indexTo === 'undefined') this.settings.items.indexTo = values.length - 1;
+    if (typeof indexFrom === 'undefined') this.state.items.indexFrom = 0;
+    if (typeof indexTo === 'undefined') this.state.items.indexTo = values.length - 1;
 
     if (indexTo > values.length - 1) {
-      this.settings.items.indexTo = values.length - 1;
+      this.state.items.indexTo = values.length - 1;
     }
 
     if (isTwoHandles) {
       if (indexFrom > indexTo) {
-        this.settings.items.indexFrom = indexTo;
+        this.state.valueFrom = this.state.valueTo;
+        this.state.items.indexFrom = indexTo;
       }
-      if (indexFrom < 0) this.settings.items.indexFrom = 0;
+      if (indexFrom < 0) this.state.items.indexFrom = 0;
     }
   }
 
   private applyDefaultValuesForUndefinedValues(): void {
-    const { items, stepValue, isTwoHandles, isVertical, isTip, minValue, maxValue, valueFrom, valueTo } = this.settings;
+    const { items, stepValue, isTwoHandles, isVertical, isTip, minValue, maxValue, valueFrom, valueTo } = this.state;
 
-    if (typeof isVertical !== 'boolean') this.settings.isVertical = TRSModel.defaults.isVertical;
-    if (typeof isTwoHandles !== 'boolean') this.settings.isTwoHandles = TRSModel.defaults.isTwoHandles;
-    if (typeof isTip !== 'boolean') this.settings.isTip = TRSModel.defaults.isTip;
-    if (Number.isNaN(minValue)) this.settings.minValue = Number(TRSModel.defaults.minValue);
-    if (Number.isNaN(maxValue)) this.settings.maxValue = Number(TRSModel.defaults.maxValue);
-    if (Number.isNaN(valueFrom)) this.settings.valueFrom = Number(TRSModel.defaults.valueFrom);
-    if (Number.isNaN(valueTo)) this.settings.valueTo = Number(TRSModel.defaults.valueTo);
-    if (Number.isNaN(stepValue)) this.settings.stepValue = TRSModel.defaults.stepValue;
-    if (typeof items !== 'object') this.settings.items = TRSModel.defaults.items;
+    if (typeof isVertical !== 'boolean') this.state.isVertical = TRSModel.defaults.isVertical;
+    if (typeof isTwoHandles !== 'boolean') this.state.isTwoHandles = TRSModel.defaults.isTwoHandles;
+    if (typeof isTip !== 'boolean') this.state.isTip = TRSModel.defaults.isTip;
+    if (Number.isNaN(minValue)) this.state.minValue = Number(TRSModel.defaults.minValue);
+    if (Number.isNaN(maxValue)) this.state.maxValue = Number(TRSModel.defaults.maxValue);
+    if (Number.isNaN(valueFrom)) this.state.valueFrom = Number(TRSModel.defaults.valueFrom);
+    if (Number.isNaN(valueTo)) this.state.valueTo = Number(TRSModel.defaults.valueTo);
+    if (Number.isNaN(stepValue)) this.state.stepValue = TRSModel.defaults.stepValue;
+    if (typeof items !== 'object') this.state.items = TRSModel.defaults.items;
   }
 
   private validateMinAndMaxValues(): void {
-    const { items } = this.settings;
-    let { valueFrom, valueTo } = this.settings;
+    const { items } = this.state;
+    let { valueFrom, valueTo } = this.state;
 
     const isUsingItems = items && items.values.length > 1;
 
-    let maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.settings.maxValue);
-    let minValue = Number(isUsingItems ? items.values[0] : this.settings.minValue);
+    let maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.state.maxValue);
+    let minValue = Number(isUsingItems ? items.values[0] : this.state.minValue);
 
     if (minValue === maxValue) {
-      this.settings.valueFrom = minValue;
-      valueFrom = this.settings.valueFrom;
-      this.settings.maxValue = minValue + 1;
-      maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.settings.maxValue);
-      this.settings.valueTo = maxValue;
+      this.state.valueFrom = minValue;
+      valueFrom = this.state.valueFrom;
+      this.state.maxValue = minValue + 1;
+      maxValue = Number(isUsingItems ? items.values[items.values?.length - 1] : this.state.maxValue);
+      this.state.valueTo = maxValue;
       valueTo = maxValue;
     }
 
     if (maxValue < minValue) {
-      this.settings.minValue = maxValue;
-      this.settings.maxValue = minValue;
+      this.state.minValue = maxValue;
+      this.state.maxValue = minValue;
       minValue = maxValue;
       maxValue = minValue;
-      if (valueFrom < minValue || valueFrom > maxValue) this.settings.valueFrom = minValue;
+      if (valueFrom < minValue || valueFrom > maxValue) this.state.valueFrom = minValue;
 
-      if (valueTo < minValue || valueTo > maxValue) this.settings.valueTo = maxValue;
+      if (valueTo < minValue || valueTo > maxValue) this.state.valueTo = maxValue;
     }
   }
 
   private validateStepValue(): void {
-    const { maxValue, minValue, stepValue } = this.settings;
+    const { maxValue, minValue, stepValue } = this.state;
 
     const size = Number(maxValue) - Number(minValue);
-    if (stepValue < 1) this.settings.stepValue = 1;
-    if (this.settings.stepValue > size) this.settings.stepValue = size;
+    if (stepValue < 1) this.state.stepValue = 1;
+    if (this.state.stepValue > size) this.state.stepValue = size;
   }
 
   private validateHandlesValue(): void {
-    const { isTwoHandles, minValue, maxValue } = this.settings;
-    let { valueFrom, valueTo } = this.settings;
+    const { isTwoHandles, minValue, maxValue } = this.state;
+    let { valueFrom, valueTo } = this.state;
 
     if (isTwoHandles) {
       if (valueFrom > valueTo) {
-        this.settings.valueFrom = valueTo;
-        this.settings.valueTo = valueFrom;
-        valueFrom = this.settings.valueFrom;
-        valueTo = this.settings.valueTo;
+        this.state.valueFrom = valueTo;
+        this.state.valueTo = valueFrom;
+        valueFrom = this.state.valueFrom;
+        valueTo = this.state.valueTo;
       }
-      if (valueTo < valueFrom) this.settings.valueTo = valueFrom;
+      if (valueTo < valueFrom) this.state.valueTo = valueFrom;
 
-      if (valueFrom < minValue) this.settings.valueFrom = minValue;
-    } else if (valueTo < minValue) this.settings.valueTo = minValue;
+      if (valueFrom < minValue) this.state.valueFrom = minValue;
+    } else if (valueTo < minValue) this.state.valueTo = minValue;
 
-    if (valueTo > maxValue) this.settings.valueTo = maxValue;
+    if (valueTo > maxValue) this.state.valueTo = maxValue;
   }
 
   private validate(): void {
@@ -200,6 +155,172 @@ class TRSModel {
 
       this.validateHandlesValue();
     }
+  }
+
+  private getIndex(relValue: number | string): number {
+    const {
+      items: { values },
+    } = this.state;
+    let index = -1;
+    if (this.isUsingItems()) {
+      for (let i = 0; i < values.length; i += 1) {
+        if (relValue.toString() === values[i].toString()) {
+          index = i;
+          break;
+        }
+      }
+    }
+    return index;
+  }
+
+  updateState(data: AnyObject): void {
+    mergeSliderOptions(this.state, data);
+    this.validate();
+  }
+
+  getState(): RangeSliderOptions {
+    return this.state;
+  }
+
+  onHandlePositionChange(data: HandleMovingResult): void {
+    const { isFromHandle, relValue } = data;
+
+    if (isFromHandle) this.setValueFrom(relValue);
+    else this.setValueTo(relValue);
+
+    const validatedRelValue = isFromHandle ? this.getValueFrom() : this.getValueTo();
+    this.state.onHandlePositionChange?.call({ ...this.state }, { isFromHandle, relValue: validatedRelValue });
+  }
+
+  getIsVertical(): boolean {
+    return this.state.isVertical;
+  }
+
+  setIsVertical(isVertical: boolean): void {
+    this.state.isVertical = isVertical;
+  }
+
+  getIsTwoHandles(): boolean {
+    return this.state.isTwoHandles;
+  }
+
+  setIsTwoHandles(isTwoHandles: boolean): void {
+    this.state.isTwoHandles = isTwoHandles;
+  }
+
+  getIsTip(): boolean {
+    return this.state.isTip;
+  }
+
+  setIsTip(isTip: boolean): void {
+    this.state.isTip = isTip;
+  }
+
+  getMinValue(): number | string {
+    return this.state.minValue;
+  }
+
+  setMinValue(newMinValue: number | string): void {
+    const newValueIndex = this.getIndex(newMinValue);
+    if (newValueIndex !== -1) this.state.minValue = this.state.items.values[newValueIndex];
+    this.validate();
+  }
+
+  getMaxValue(): number | string {
+    return this.state.maxValue;
+  }
+
+  setMaxValue(newMaxValue: number | string): void {
+    const newValueIndex = this.getIndex(newMaxValue);
+    if (newValueIndex !== -1) {
+      this.state.maxValue = this.state.items.values[newValueIndex];
+      this.validate();
+    }
+  }
+
+  getStepValue(): number {
+    return this.state.stepValue;
+  }
+
+  setStepValue(newStepValue: number): void {
+    this.state.stepValue = newStepValue;
+    this.validate();
+  }
+
+  getValueFrom(): number | string {
+    return this.state.valueFrom;
+  }
+
+  setValueFrom(newValueFrom: number | string): void {
+    if (this.isUsingItems()) {
+      const newValueIndex = this.getIndex(newValueFrom);
+      if (newValueIndex !== -1) {
+        this.state.valueFrom = this.state.items.values[newValueIndex];
+        this.state.items.indexFrom = newValueIndex;
+      }
+    } else this.state.valueFrom = newValueFrom;
+    this.validate();
+  }
+
+  getValueTo(): number | string {
+    return this.state.valueTo;
+  }
+
+  setValueTo(newValueTo: number | string): void {
+    if (this.isUsingItems()) {
+      const newValueIndex = this.getIndex(newValueTo);
+      if (newValueIndex !== -1) {
+        this.state.valueTo = this.state.items.values[newValueIndex];
+        this.state.items.indexTo = newValueIndex;
+      }
+    } else this.state.valueTo = newValueTo;
+    this.validate();
+  }
+
+  getItems(): RangeSliderItems {
+    return this.state.items;
+  }
+
+  setItems(newItems: RangeSliderItems): void {
+    this.state.items = { ...newItems };
+    this.validate();
+  }
+
+  getItemsValues(): (number | string)[] {
+    return this.state.items?.values;
+  }
+
+  setItemsValues(values: (number | string)[]): void {
+    this.state.items.values = values;
+    this.validate();
+  }
+
+  getIndexFrom(): number {
+    return this.state.items?.indexFrom;
+  }
+
+  setIndexFrom(index: number): void {
+    if (this.state.items) {
+      this.state.items.indexFrom = index;
+      if (this.isUsingItems()) this.state.valueFrom = this.state.items.values[index];
+      this.validate();
+    }
+  }
+
+  getIndexTo(): number {
+    return this.state.items?.indexTo;
+  }
+
+  setIndexTo(index: number): void {
+    if (this.state.items) {
+      this.state.items.indexTo = index;
+      if (this.isUsingItems()) this.state.valueTo = this.state.items.values[index];
+      this.validate();
+    }
+  }
+
+  isUsingItems(): boolean {
+    return this.state.items?.values?.length > 1;
   }
 }
 
--- SRC/components/toxin-rangeslider/core/presenter.ts
+++ SRC/components/toxin-rangeslider/core/presenter.ts
@@ -18,7 +18,6 @@ class TRSPresenter {
   }
 
   private notifyModel(data: HandleMovingResult): void {
-    this.model.updateHandleState(data);
     this.model.onHandlePositionChange(data);
     this.view.drawSlider(this.getState());
   }
@@ -30,6 +29,106 @@ class TRSPresenter {
   update(data = {}): void {
     this.model.updateState(data);
     this.view.drawSlider(this.getState());
+  }
+
+  getIsVertical(): boolean {
+    return this.model.getIsVertical();
+  }
+
+  setIsVertical(isVertical: boolean): void {
+    this.model.setIsVertical(isVertical);
+  }
+
+  getIsTwoHandles(): boolean {
+    return this.model.getIsTwoHandles();
+  }
+
+  setIsTwoHandles(isTwoHandles: boolean): void {
+    this.model.setIsTwoHandles(isTwoHandles);
+  }
+
+  getIsTip(): boolean {
+    return this.model.getIsTip();
+  }
+
+  setIsTip(isTip: boolean): void {
+    this.setIsTip(isTip);
+  }
+
+  getMinValue(): number | string {
+    return this.model.getMinValue();
+  }
+
+  setMinValue(newMinValue: number | string): void {
+    this.setMinValue(newMinValue);
+  }
+
+  getMaxValue(): number | string {
+    return this.model.getMaxValue();
+  }
+
+  setMaxValue(newMaxValue: number | string): void {
+    this.model.setMaxValue(newMaxValue);
+  }
+
+  getStepValue(): number {
+    return this.getStepValue();
+  }
+
+  setStepValue(newStepValue: number): void {
+    this.setStepValue(newStepValue);
+  }
+
+  getValueFrom(): number | string {
+    return this.getValueFrom();
+  }
+
+  setValueFrom(newValueFrom: number | string): void {
+    this.setValueFrom(newValueFrom);
+  }
+
+  getValueTo(): number | string {
+    return this.model.getValueTo();
+  }
+
+  setValueTo(newValueTo: number | string): void {
+    this.model.setValueTo(newValueTo);
+  }
+
+  getItems(): RangeSliderItems {
+    return this.model.getItems();
+  }
+
+  setItems(newItems: RangeSliderItems): void {
+    this.model.setItems(newItems);
+  }
+
+  getItemsValues(): (number | string)[] {
+    return this.model.getItemsValues();
+  }
+
+  setItemsValues(values: (number | string)[]): void {
+    this.model.setItemsValues(values);
+  }
+
+  getIndexFrom(): number {
+    return this.model.getIndexFrom();
+  }
+
+  setIndexFrom(index: number): void {
+    this.model.setIndexFrom(index);
+  }
+
+  getIndexTo(): number {
+    return this.model.getIndexTo();
+  }
+
+  setIndexTo(index: number): void {
+    this.model.setIndexTo(index);
+  }
+
+  isUsingItems(): boolean {
+    return this.model.isUsingItems();
   }
 }
 
